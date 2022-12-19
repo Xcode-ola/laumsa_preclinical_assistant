0feb12066da090d0fe48fb567e6d6a22e06dd82e
Xcode-ola <adeolaademilua57@gmail.com>
2022-12-19 08:35:28 +0100

    second commit

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Courses)
+admin.site.register(Quiz)
+admin.site.register(PracticeQuestions)
+admin.site.register(Chapters)
+admin.site.register(Summary)
+
+class StemInLineTable(admin.TabularInline):
+    model = QuestionStem
+    fields = [
+        'body',
+        'ans',
+    ]
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = [
+        'quiz',
+        'question',
+    ]
+    list_display = [
+        'quiz',
+        'question',
+    ]
+    inlines = [
+        StemInLineTable,
+    ]
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = [
+        'question',
+        'body',
+        'ans'
+    ]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(QuestionStem, AnswerAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+class Courses(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "Course"
+        verbose_name_plural = "Courses"
+
+    def __str__(self):
+        return self.name
+
+class Chapters(models.Model):
+    course = models.ForeignKey(Courses, related_name="chapter", on_delete=models.CASCADE)
+    name = models.CharField(max_length=500)
+    slug = models.SlugField()
+
+    class Meta:
+        verbose_name = "Chapter"
+        verbose_name_plural = "Chapters"
+
+    def __str__(self):
+        return self.name
+
+class Summary(models.Model):
+    course = models.ForeignKey(Courses, related_name="summary", on_delete=models.CASCADE)
+    chapter = models.ForeignKey(Chapters, related_name="summary", on_delete=models.CASCADE)
+    body = RichTextField()
+    isverified = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Summaries"
+
+    def __str__(self):
+        return self.course.name
+
+class Quiz(models.Model):
+    course = models.ForeignKey(Courses, related_name="quiz", on_delete=models.CASCADE)
+    name = models.CharField(max_length=2000)
+
+    class Meta:
+        verbose_name = "Quiz"
+        verbose_name_plural = "Quizzes"
+
+    def __str__(self):
+        return self.name
+
+class Question(models.Model):
+    course = models.ForeignKey(Courses, related_name="question", on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, related_name="question", on_delete=models.CASCADE)
+    question = models.TextField()
+
+    def __str__(self):
+        return self.quiz.name
+
+class QuestionStem(models.Model):
+    question = models.ForeignKey(Question, related_name="stem", on_delete=models.CASCADE)
+    body = models.CharField(max_length=20000)
+    ans = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.question.name
+
+class PracticeQuestions(models.Model):
+    course = models.ForeignKey(Courses, related_name="theory", on_delete=models.CASCADE)
+    chapter = models.ForeignKey(Chapters, related_name="theory", on_delete=models.CASCADE)
+    question = models.TextField()
+    image1 = models.URLField(blank=True, null=True)
+    image2 = models.URLField(blank=True, null=True)
+    image3 = models.URLField(blank=True, null=True)
+    explanation = RichTextField()
+
+    class Meta:
+        verbose_name = "Theory"
+        verbose_name_plural = "Theories"
+
+    def __str__(self):
+        return self.course.name
+
